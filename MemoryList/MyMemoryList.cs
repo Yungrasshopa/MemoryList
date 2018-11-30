7b65b9649362c9d396a81f7b21ae2a39e851527a
John Pot <j.pot@digipen.edu>
2018-11-30 10:14:41 -0600

    Working on implementation.

--- MemoryList/MyMemoryList.cs
+++ MemoryList/MyMemoryList.cs
@@ -1,9 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MemoryList
 {
-    public class MyMemoryList
+    public class MyMemoryList<T> where T: new()
     {
+        private List<T> _list = new List<T>();
+
+        private List<T> _oldList = new List<T>();
+
+        private int _length;
+        public int Length
+        {
+            get => _list.Count;
+            set
+            {
+                _length = value;
+            }
+        }
+        
+
+        public void Add(T item)
+        {
+            _list.Add(item);
+            _oldList.Add(item);
+        }
+
+        public void Remove(T item)
+        {
+            _list.Remove(item);
+        }
+
+        public bool Contained(T item)
+        {
+            return _oldList.Contains(item);
+        }
+
+
 
     }
 }
